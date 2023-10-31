b45115c9ff87bfc5f4fead0dc4da53617c35d971
Dmytro Berezhnyi <120669524+dimkakao@users.noreply.github.com>
2023-10-31 11:35:49 +0200

    Final Version

--- task.rb
+++ task.rb
@@ -1,3 +1,7 @@
+#Бережний Дмитро КС31
+#Шліхутка Іван КС31
+require 'set'
+
 class Edge
   attr_accessor :from, :to, :weight
 
@@ -16,48 +20,35 @@ class Edge
   end
 
   def to_s
-    "from " + from.to_s + " to " + to.to_s + " weight " + weight.to_s
+    "From " + @from.to_s + " To " + @to.to_s + " Weight " + @weight.to_s
   end
 end
 
-class DisjointSet
+class GraphSubset
   attr_accessor :parent, :rank
 
   def initialize
     @parent = self
     @rank = 0
   end
-
-  def to_s
-    "rank " + rank.to_s + " parent id " + @id.to_s + "\t".ljust(15) + super.to_s
-  end
 end
 
 def kruskal(vertices, edges_array)
-  edges_array.sort_by! { |edge|
-    edge.weight
-  }
-  puts edges_array
-  puts
+  edges_array.sort_by! { |edge| edge.weight }
 
   minimum_spanning_tree = []
-
-  # disjoint_sets = Array.new(vertices) {
-  #   DisjointSet.new
-  # }
-  disjoint_sets = (0...vertices).each_with_object({}) do |index, map|
-    map[index] = DisjointSet.new
-  end
+  graph_subsets = Array.new(vertices) {
+    GraphSubset.new
+  }
 
   edges_array.each do |edge|
-    point_from_set = find_set(disjoint_sets[edge.from])
-    # puts "from_set === " + point_from_set.to_s
-    point_to_set = find_set(disjoint_sets[edge.to])
-    # puts "to_set === " + point_to_set.to_s
 
-    if point_from_set != point_to_set
+    from_point_set = find_set(graph_subsets[edge.from])
+    to_point_set = find_set(graph_subsets[edge.to])
+
+    if from_point_set != to_point_set
       minimum_spanning_tree << edge
-      union_sets(point_from_set, point_to_set)
+      union_sets(from_point_set, to_point_set)
     end
   end
 
@@ -66,16 +57,9 @@ end
 
 def find_set(node)
   node.parent
-  # puts "this ".ljust(10) + node.to_s
-  # if node != node.parent
-  #   puts "this parent -> " + node.parent.to_s
-  #   node.parent = find_set(node.parent)
-  # end
-  # node.parent
 end
 
 def union_sets(first_set, second_set)
-  puts "union!"
   if first_set.rank > second_set.rank
     second_set.parent = first_set
   else
@@ -86,43 +70,38 @@ def union_sets(first_set, second_set)
   end
 end
 
-# Приклад використання
-puts "Введіть кількість вершин (vertices):"
-
+###### Приклад виконання
+puts "Введіть максимальну кількість вершин (vertices):"
 vertices = gets.chomp.to_i
+
 while vertices <= 0 || vertices > 100
-  puts "Введено не коректну кількість точок. Спробуйте знову."
+  puts "Введено некоректну кількість точок. Спробуйте знову."
   puts "Введіть кількість вершин (vertices):"
   vertices = gets.chomp.to_i
 end
-# vertices = 3
-# edges = [Edge.new(0, 1, 1),
-#          Edge.new(2, 0, 4)]
-
-edges_set = Set.new
 
 counter = 0
-loop do
+edges_set = Set.new
 
+loop do
   puts "Введіть ребро #{counter} у форматі 'vertex_from vertex_to weight' для кожного ребра (наприклад, '1 2 5') або 'end' для завершення:"
   input = gets.chomp
 
   break if input.downcase == 'end'
 
-  values = input.split.map(&:to_i).take(3) # Отримання лише перших двох чисел
-  while values.size < 3 || values.all? { |vertex| vertex < 0 } || values.take(2).all? { |vertex| vertex >= vertices }
+  values = input.split.map(&:to_i).take(3)
+  while values.size != 3 || values.any? { |vertex| vertex < 0 } || values[0, 2].any? { |vertex| vertex >= vertices }
     puts "Введено некоректне ребро. Спробуйте знову."
-    puts "Ребро #{counter}: "
-
     input = gets.chomp
     break if input.downcase == 'end'
     values = input.split.map(&:to_i).take(3)
   end
 
-  from, to = values.take(2) # Беремо тільки перші два числа
-  weight = values[2] # Беремо тільки третє число
+  break if input.downcase == 'end'
 
+  from, to, weight = values
   edge = Edge.new(from, to, weight)
+
   if edges_set.include?(edge)
     puts "Ребро вже присутнє в множині."
   else
@@ -131,23 +110,14 @@ loop do
   end
 end
 
-puts "Результат:"
-puts edges_set.size
+puts "Ребра:"
 edges_set.each_with_index do |edge, index|
   puts "Ребро #{index}: from #{edge.from} to #{edge.to}, вага #{edge.weight}"
 end
 
-# edges = [Edge.new(0, 1, 3),
-#          Edge.new(0, 2, 5),
-#          Edge.new(1, 2, 2),
-#          Edge.new(1, 3, 1),
-#          Edge.new(2, 3, 1),
-#          Edge.new(3, 4, 3),
-#          Edge.new(4, 1, 2)]
-
 minimum_spanning_tree = kruskal(vertices, edges_set.to_a)
 
-puts "Minimum Spanning Tree:"
+puts "\nMinimum Spanning Tree:\n"
 minimum_spanning_tree.each do |edge|
   puts "Edge: #{edge.from} - #{edge.to}, Weight: #{edge.weight}"
 end
